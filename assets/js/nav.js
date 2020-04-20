c96c2f3712ad55b1cc4afdb15e53d9f9f7197e69
muhammedashfar7 <muhammed.ashfar.7@gmail.com>
2020-04-20 17:18:05 +0300

    update

--- assets/js/nav.js
+++ assets/js/nav.js
@@ -1,9 +1,10 @@
-$(document).ready(function () {
-    $(function () {
-        $('li a').click(function (e) {
-            e.preventDefault();
-            $('a').removeClass('active');
-            $(this).addClass('active');
-        });
-    });
-});
+$(function(){
+    var current = location.pathname;
+    $('#nav li a').each(function(){
+        var $this = $(this);
+        // if the current path is like this link, make it active
+        if($this.attr('href').indexOf(current) !== -1){
+            $this.addClass('active');
+        }
+    })
+})
